931c44f8ff829ae984f157377fa8e86f9a5ff81a
istiak19 <istiak15-14128@diu.edu.bd>
2025-02-07 21:51:49 +0600

    send image text

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -4,8 +4,8 @@ import { AuthContext } from "../provider/AuthProvider";
 import Swal from "sweetalert2";
 import axios from "axios";
 
-const image_key = import.meta.env.VITE_IMGBB_apiKey;
-const image_api = `https://api.imgbb.com/1/upload?key=${image_key}`
+// const image_key = import.meta.env.VITE_IMGBB_apiKey;
+// const image_api = `https://api.imgbb.com/1/upload?key=${image_key}`
 
 const Create = () => {
   const { user, login } = useContext(AuthContext);
@@ -48,29 +48,6 @@ const Create = () => {
     }
   };
 
-  const getBuffer = async (prompt, category) => {
-    const finalPrompt = `imagine a ${category} : ${prompt}`;
-    const myForm = new FormData()
-    myForm.append('prompt', finalPrompt);
-    const response = await fetch('https://clipdrop-api.co/text-to-image/v1', {
-      method: 'POST',
-      headers: {
-        'x-api-key': import.meta.env.VITE_CD_apiKey,
-      },
-      body: myForm,
-    })
-    const buffer = response.arrayBuffer();
-    console.log(buffer);
-    return buffer;
-  }
-
-  const imageGenerateURL = async (buffer) => {
-    const formData = new FormData();
-    formData.append('image', new Blob([buffer], { type: "image/jpeg" }),
-      `${prompt}.jpg`);
-    const res = await axios.post(image_api, formData);
-    return res.data.data.url;
-  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -104,12 +81,23 @@ const Create = () => {
     //validation End
 
     console.log({ prompt, category });
-    const buffer = await getBuffer(prompt, category)
-    const photo = await imageGenerateURL(buffer);
-    console.log("Image URL:", photo);
-    // const blob = new Blob([buffer], { type: "image/jpeg" });
-    // const imageUrl = URL.createObjectURL(blob);
-    // console.log("Image URL:---->", imageUrl);
+    const dataInfo = {
+      email: user?.email,
+      prompt: prompt,
+      userName: user?.displayName || 'Anonymous',
+      userImg: user?.photoURL || 'https://img.icons8.com/?size=48&id=108294&format=png',
+      category: category
+    };
+    const res = await axios.post(
+      'http://localhost:5000/create-image',
+      dataInfo,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(res.data)
   };
 
   return (
